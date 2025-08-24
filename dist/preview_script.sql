037b70bdecf7593c83e0593407e62c72a1ba77c3
MD. MONIR HOSSAIN <monir@erainfotechbd.com>
2025-08-24 15:53:18 +0600

    preview before deployment file added

---
+++ dist/preview_script.sql
@@ -0,0 +1,23 @@
+-- SQLcl uses the SQLCL engine for formatted sql changelog not the JDBC engine
+-- By default, a project will not use substitution variables and allows blank
+-- lines in sql statements.
+
+set define off
+set sqlblanklines on
+
+-- Prechecks modifiable helper
+-- Check running with SQLcl
+-- Check minimum DB version
+-- Check character set
+-- @utils/prechecks.sql
+
+-- SLOT
+-- Custom pre Liquibase code here (perhaps creation of a schema)
+-- This is MINIMAL pre setup, everything that can go through Liquibase - SHOULD
+
+-- Kick off Liquibase
+prompt "Generate SQL preview file "
+lb update-sql -log -changelog-file releases/main.changelog.xml -search-path "." -output-file preview.sql
+
+
+--@utils/recompile.sql
